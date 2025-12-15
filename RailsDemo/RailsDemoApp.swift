58a29be572191ac043d06e619c559e67dc40fdb7
mallardknundrum <mallardknundrum@gmail.com>
2025-12-15 17:41:17 -0500

    Initial Commit

---
+++ RailsDemo/RailsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailsDemoApp.swift
+//  RailsDemo
+//
+//  Created by Jeremiah Hawks on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct RailsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RailsDemoTests/RailsDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RailsDemoTests.swift
+//  RailsDemoTests
+//
+//  Created by Jeremiah Hawks on 12/15/25.
+//
+
+import Testing
+@testable import RailsDemo
+
+struct RailsDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
